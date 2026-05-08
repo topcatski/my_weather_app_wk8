dafbb1e26a161a548e643e797004a935363dcddb
Monica Boland <monicaboland2000@gmail.com>
2026-05-08 18:12:43 +0100

    Added call to axios in function searchCity

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
 function searchCity(city) {
   //make api call and update the interface
   //separation of concerns
   let apiKey = "8717badd797bf40d3ca2to48c422f094";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-  console.log(apiUrl);
+  axios.get(apiUrl).then(refreshWeather);
 }
 
 function handleSearchSubmit(event) {
