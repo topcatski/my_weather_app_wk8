57d5535dbda187bf89afbc144e56989eda05efb0
Monica Boland <monicaboland2000@gmail.com>
2026-05-08 17:52:52 +0100

    Added apiKey and apiUrl to index.js with test for url

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
+function searchCity(city) {
+  //make api call and update the interface
+  //separation of concerns
+  let apiKey = "8717badd797bf40d3ca2to48c422f094";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  console.log(apiUrl);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
